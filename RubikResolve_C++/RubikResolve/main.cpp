fa5adf8a9c4627352639270d5cc3af6ee2289d92
jcampbell-mst <campbelljake875@gmail.com>
2025-04-04 16:16:30 +0100

    basic main functionality

--- RubikResolve_C++/RubikResolve/main.cpp
+++ RubikResolve_C++/RubikResolve/main.cpp
@@ -8,12 +8,17 @@
 cube active_cube_rep = DEFAULT_CUBE_CONTENT;
 
 int main(int argc, char* argv[]) {
-	std::cout << "Hello World\n";
+	int face_index;
+	std::cout << "Select face: ";
+	std::cin >> face_index;
 
-	std::cout << grid_to_str(active_cube_rep[5][3]);
-	
-	int n;
-	std::cin >> n;
+	int grid_index;
+	std::cout << "Select grid position: ";
+	std::cin >> grid_index;
+
+	std::cout << grid_to_str(active_cube_rep[face_index][grid_index]);
+
+	std::cin.get();
 
 	return EXIT_SUCCESS;
 }
